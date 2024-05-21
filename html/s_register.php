e6cbeef7e6ad2fdc2a9cf287cdab64bcb789824e
yuenliu <101539991+yuenliu@users.noreply.github.com>
2024-05-21 23:01:42 +0800

    Update s_register.php

--- html/s_register.php
+++ html/s_register.php
@@ -10,10 +10,9 @@ echo "<h1>Hello! Welcome 店家".$store."!</h1><br>".
     "店家名稱是：".$_POST["store"]."<br>".
     "負責人姓名是：".$_POST["st-name"]."<br>".
     "負責人手機號碼是：".$_POST["phone"]."<br>".
-    "店家帳號是：".$_POST["account"]."<br>".
-    "您的E-mail是：".$_POST["email"]."<br>".
     "店家電話是：".$_POST["call"]."<br>".
     "店家地址是：".$_POST["address"]."<br>".
+    "您的E-mail是：".$_POST["email"]."<br>".
     "您的密碼是：".$_POST["psw"]."<br>".
     "確認密碼：".$_POST["psw-repeat"]."<br>";
 ?>
