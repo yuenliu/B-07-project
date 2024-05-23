a7d51dad548b0ffba108c5ee742a1ecb2b72853e
a25458417584 <101554283+a25458417584@users.noreply.github.com>
2024-05-23 16:11:43 +0800

    Add files via upload

---
+++ SQL/order.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： localhost
+-- 產生時間： 
+-- 伺服器版本： 8.0.17
+-- PHP 版本： 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `order`
+--
+
+CREATE TABLE `order` (
+  `訂單編號` varchar(6) NOT NULL,
+  `會員編號` varchar(5) NOT NULL,
+  `店家編號` varchar(5) NOT NULL,
+  `商品編號` varchar(5) NOT NULL,
+  `建立時間` date NOT NULL,
+  `預計取餐時間` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`訂單編號`),
+  ADD KEY `consuemr.``會員編號``` (`會員編號`),
+  ADD KEY `store.``店家編號``` (`店家編號`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
