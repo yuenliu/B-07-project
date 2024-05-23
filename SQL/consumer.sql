17b169311ddd880ed615df8645d040426f22322d
a25458417584 <101554283+a25458417584@users.noreply.github.com>
2024-05-23 16:06:16 +0800

    Add files via upload

---
+++ SQL/consumer.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： localhost
+-- 產生時間： 
+-- 伺服器版本： 8.0.17
+-- PHP 版本： 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `consumer`
+--
+
+CREATE TABLE `consumer` (
+  `會員編號` varchar(5) NOT NULL,
+  `姓名` varchar(10) NOT NULL,
+  `性別` varchar(1) NOT NULL DEFAULT '男',
+  `來自縣市` varchar(5) NOT NULL DEFAULT '台北市',
+  `E-mail` varchar(50) NOT NULL,
+  `密碼` varchar(50) NOT NULL,
+  `手機號碼` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `consumer`
+--
+ALTER TABLE `consumer`
+  ADD PRIMARY KEY (`會員編號`),
+  ADD UNIQUE KEY `密碼` (`密碼`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ SQL/store.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： localhost
+-- 產生時間： 
+-- 伺服器版本： 8.0.17
+-- PHP 版本： 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `store`
+--
+
+CREATE TABLE `store` (
+  `店家編號` varchar(5) NOT NULL,
+  `負責人姓名` varchar(5) NOT NULL,
+  `負責人手機號碼` varchar(10) NOT NULL,
+  `E-mail` varchar(50) NOT NULL,
+  `店家電話` varchar(10) NOT NULL,
+  `店家地址` varchar(50) NOT NULL,
+  `密碼` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `store`
+--
+ALTER TABLE `store`
+  ADD PRIMARY KEY (`店家編號`),
+  ADD UNIQUE KEY `負責人手機號碼` (`負責人手機號碼`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
