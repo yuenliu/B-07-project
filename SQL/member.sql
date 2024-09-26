061ada3eec90986f758cb492f57c0a165c8dc827
a25458417584 <101554283+a25458417584@users.noreply.github.com>
2024-09-26 17:05:05 +0800

    Update member.sql

--- SQL/member.sql
+++ SQL/member.sql
@@ -30,8 +30,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `member` (
   `id` int(5) NOT NULL,
+  `identity` varchar(8) NOT NULL COMMENT '身分',
   `account` varchar(20) NOT NULL COMMENT '帳號',
-  `password` varchar(100) NOT NULL COMMENT '密碼',
+  `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密碼',
   `gender` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '顧客性別',
   `name` varchar(10) NOT NULL COMMENT '(負責人)姓名',
   `phoneNumber` varchar(10) NOT NULL COMMENT '(負責人)電話號碼',
@@ -40,6 +41,15 @@ CREATE TABLE `member` (
   `storePhoneNumber` varchar(13) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '店家電話',
   `storeAddress` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '店家地址'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- 傾印資料表的資料 `member`
+--
+
+INSERT INTO `member` (`id`, `identity`, `account`, `password`, `gender`, `name`, `phoneNumber`, `E-mail`, `storeName`, `storePhoneNumber`, `storeAddress`) VALUES
+(1, 'root', 'root', '$2y$10$Ewv5tPIzc/LCyb7uXYU6puF8M61gjlVC2HjuTns4zZeGfENrVVGJ6', '男', 'root', '123', '123@123.com', NULL, NULL, NULL),
+(2, 'consumer', 'a25458417584', '$2y$10$ZjMbIFe/WTennR0LvO8LAOzn17.BIyOlr0ugcoogQVEBGR4oK1elK', '男', '黃俊嘉', '0968795335', 'nht1497@gmail.com', NULL, NULL, NULL),
+(3, 'store', 'a123456789', '$2y$10$D0q3DAPkCXQFhaMP488bVObkHDEIpJ6dq5qx5Bgfdp2UvZf4c3/Hy', NULL, '乾你屁事', '0912345678', 'nsfgkjndkfjgn@gmail.com', '測試', '(02)1234-5678', '乾你屁事');
 
 --
 -- 已傾印資料表的索引
@@ -59,7 +69,7 @@ ALTER TABLE `member`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `member`
 --
 ALTER TABLE `member`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
