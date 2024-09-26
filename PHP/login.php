2ebe2a7e9e2c34d806c80ef7eb68d36f9febccb8
yuenliu <101539991+yuenliu@users.noreply.github.com>
2024-09-26 14:38:38 +0800

    Update login.php

--- PHP/login.php
+++ PHP/login.php
@@ -15,7 +15,7 @@ if (isset($_SESSION["account"]) || $_SESSION["account"] != "") {
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/login1.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 
 <body>
