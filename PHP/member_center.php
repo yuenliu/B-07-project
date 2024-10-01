ab2a78e1fffb0d21dce3350305b8f6ac2c5ed4f0
a25458417584 <101554283+a25458417584@users.noreply.github.com>
2024-10-02 01:09:07 +0800

    Update member_center.php

--- PHP/member_center.php
+++ PHP/member_center.php
@@ -1,6 +1,6 @@
 <?php
-    session_start();
-    require_once("login_check.php");
+session_start();
+require_once("login_check.php");
 ?>
 
 <html>
@@ -13,38 +13,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/login1.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js
+"></script>
+    <link rel="stylesheet" href="css/member_center.css">
 </head>
 
 </html>
 
 <body>
-    <nav class="navbar navbar-inverse fixed-top navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <div class="navbar-header">
-                <a class="navbar-brand text-white">文大線上點餐系統</a>
-            </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="nav-item"><a href="home.html">首頁</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="contact.html"> 聯絡管理員</a></li>
-                    <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> 登出</a></li>
-                </ul>
+    <?php
+    include("navbar.php");
+    ?>
+    
+    <aside class="col-lg-1">
+        <ul class="nav nav-pills nav-stacked">
+            <li><a href="member_change_password.php">修改密碼</a></li>
+        </ul>
+    </aside>
+    <div class="col-sm-6 col-sm-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">儀錶板</div>
+            <div class="panel-body">
+                <?php
+                require_once("database.php");
+                session_start();
+                //查詢登入會員資料
+                $query_RecMember = "SELECT * FROM `member` WHERE `account`='" . $_SESSION["account"] . "'";
+                $RecMember = mysqli_query($conn, $query_RecMember);
+                $row_Recmember = mysqli_fetch_assoc($RecMember);
+
+                if ($row_Recmember["identity"] == "consumer") {
+                    echo "<p>會員名稱：<strong>";
+                    echo $row_Recmember["name"];
+                    echo "</strong></p>";
+                } else if ($row_Recmember["identity"] == "store") {
+                    echo "<p>店家名稱：<strong>";
+                    echo $row_Recmember["name"];
+                    echo "</strong></p>";
+                } else if ($row_Recmember["identity"] == "root") {
+                    echo "<p>管理員歡迎回來!</p>";
+                }
+                ?>
             </div>
         </div>
-    </nav>
-
-    <h1>測試畫面，你看到這頁就表示你登入成功了</h1>
-    <?php
-    require_once("database.php");
-    session_start();
-    //查詢登入會員資料
-    $query_RecMember = "SELECT * FROM `member` WHERE `account`='" . $_SESSION["account"] . "'";
-    $RecMember = mysqli_query($conn,$query_RecMember);
-    $row_Recmember = mysqli_fetch_assoc($RecMember);
-    ?>
-    <p>會員名稱：<strong><?php echo $row_Recmember["account"]; ?></strong></p>
+    </div>
 </body>
