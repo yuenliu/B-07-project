37efd81ddce2aab2d063ca73b7d9f750d858c805
yuenliu <101539991+yuenliu@users.noreply.github.com>
2024-10-02 03:47:25 +0800

    Add files via upload

--- PHP/member_center.php
+++ PHP/member_center.php
@@ -1,8 +1,11 @@
 <?php
-session_start();
-require_once("login_check.php");
+    include("navbar.php");
+    session_start();
+    require_once("login_check.php");
 ?>
 
+
+<!DOCTYPE html>
 <html>
 
 <head>
@@ -13,37 +16,28 @@ require_once("login_check.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js
-"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/member_center.css">
 </head>
 
-</html>
-
 <body>
-    <?php
-    include("navbar.php");
-    ?>
-    
-    <aside class="col-lg-1">
-        <ul class="nav nav-pills nav-stacked">
-            <li><a href="member_change_password.php">修改密碼</a></li>
-        </ul>
-    </aside>
-    <div class="col-sm-6 col-sm-offset-2">
-        <div class="panel panel-default">
-            <div class="panel-heading">儀錶板</div>
-            <div class="panel-body">
+        <!--內容-->
+        <div class="col-sm-4 col-sm-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h1>會員中心</h1></div>
+                <div class="panel-body">
                 <?php
                 require_once("database.php");
                 session_start();
                 //查詢登入會員資料
                 $query_RecMember = "SELECT * FROM `member` WHERE `account`='" . $_SESSION["account"] . "'";
-                $RecMember = mysqli_query($conn, $query_RecMember);
+                $RecMember = mysqli_query($conn,$query_RecMember);
                 $row_Recmember = mysqli_fetch_assoc($RecMember);
-
+                
                 if ($row_Recmember["identity"] == "consumer") {
-                    echo "<p>會員名稱：<strong>";
+                    echo "<p><h2>會員名稱：<strong>";
                     echo $row_Recmember["name"];
                     echo "</strong></p>";
                 } else if ($row_Recmember["identity"] == "store") {
@@ -54,7 +48,22 @@ require_once("login_check.php");
                     echo "<p>管理員歡迎回來!</p>";
                 }
                 ?>
+                <br>
+                <p align="center">
+                <a href="member_updateform.php"> 修改會員資料 </a>
+                <br><br>
+
+                <a href="member_change_password.php"> 修改會員密碼 </a>
+                <br><br>
+                <a href="logout.php"> 登出系統 </a><br>
+                </p>
+                </div>
+                </div>
             </div>
         </div>
-    </div>
+    <!-- javascript -->
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
+
+</html>
