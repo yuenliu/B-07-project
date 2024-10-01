8e3e7a616e4ddfabfe8d66b36994ebf0c646d5a9
a25458417584 <101554283+a25458417584@users.noreply.github.com>
2024-10-02 04:02:54 +0800

    Update member_center.php

--- PHP/member_center.php
+++ PHP/member_center.php
@@ -1,11 +1,8 @@
 <?php
-    include("navbar.php");
-    session_start();
-    require_once("login_check.php");
+session_start();
+require_once("login_check.php");
 ?>
 
-
-<!DOCTYPE html>
 <html>
 
 <head>
@@ -16,26 +13,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js
+"></script>
     <link rel="stylesheet" href="css/member_center.css">
 </head>
 
+</html>
+
 <body>
-        <!--內容-->
-        <div class="col-sm-4 col-sm-offset-4">
-            <div class="panel panel-default">
-                <div class="panel-heading"><h1>會員中心</h1></div>
-                <div class="panel-body">
+    <?php
+    include("navbar.php");
+    ?>
+    <div class="col-sm-4 col-sm-offset-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h1>
+                    <?php
+                    require_once("database.php");
+                    session_start();
+                    $query_RecMember = "SELECT * FROM `member` WHERE `account`='" . $_SESSION["account"] . "'";
+                    $RecMember = mysqli_query($conn, $query_RecMember);
+                    $row_Recmember = mysqli_fetch_assoc($RecMember);
+
+                    if ($row_Recmember["identity"] == "consumer") {
+                        echo "會員中心";
+                    } else if ($row_Recmember["identity"] == "store") {
+                        echo "店家中心";
+                    } else if ($row_Recmember["identity"] == "root") {
+                        echo "儀錶板";
+                    }
+                    ?>
+                </h1>
+            </div>
+            <div class="panel-body">
                 <?php
-                require_once("database.php");
-                session_start();
-                //查詢登入會員資料
-                $query_RecMember = "SELECT * FROM `member` WHERE `account`='" . $_SESSION["account"] . "'";
-                $RecMember = mysqli_query($conn,$query_RecMember);
-                $row_Recmember = mysqli_fetch_assoc($RecMember);
-                
                 if ($row_Recmember["identity"] == "consumer") {
                     echo "<p><h2>會員名稱：<strong>";
                     echo $row_Recmember["name"];
@@ -50,20 +61,19 @@
                 ?>
                 <br>
                 <p align="center">
-                <a href="member_updateform.php"> 修改會員資料 </a>
-                <br><br>
-
-                <a href="member_change_password.php"> 修改會員密碼 </a>
-                <br><br>
-                <a href="logout.php"> 登出系統 </a><br>
+                    <?php
+                        if ($row_Recmember["identity"] == "consumer") {
+                            echo "<a href='member_updateform.php'> 修改會員資料 </a>";
+                        } else if ($row_Recmember["identity"] == "store") {
+                            echo "<a href='store_updateform.php'> 修改店家資料 </a>";
+                        }
+                    ?>
+                    <br><br>
+                    <a href="member_change_password.php"> 修改密碼 </a>
+                    <br><br>
+                    <a href="logout.php"> 登出系統 </a><br>
                 </p>
-                </div>
-                </div>
             </div>
         </div>
-    <!-- javascript -->
-    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    </div>
 </body>
-
-</html>
