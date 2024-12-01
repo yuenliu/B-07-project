2459a2a9ef5be8af5b506478da900b36cec3623c
yuenliu <101539991+yuenliu@users.noreply.github.com>
2024-12-02 01:24:22 +0800

    Update home.php

--- PHP/home.php
+++ PHP/home.php
@@ -15,47 +15,48 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.css">
-
+    <link rel="stylesheet" href="css/login.css">
+    <style>
+    marquee {
+      font-size: 28px;  
+      font-weight: bold;    
+      color: #800;          
+      font-family: 'Microsoft JhengHei', '微軟正黑體', sans-serif;
+    }
+  </style>
 </head>
 
 <body>
     <div>
+
+        <marquee>歡迎來到文大線上點餐系統 &nbsp; 尚未註冊的顧客、店家，麻煩請先註冊~ </marquee>
         <!--內容-->
         <div class="container">
-            <header class="bgimage">
-                <div class="container">
-                    <h1 align="center">歡迎使用文大線上點餐系統</b></h1>
-                </div>
-            </header>
-            <div class="clearfix visible-xs visible-lg"></div>
+            
+            <div class="clearfix visible-md visible-lg"></div>
             <div class="row">
-                <div class="col-lg-6 col-xs-8">
-                    <div class="thumbnail">
+                <div class="col-lg-16 col-xs-18">
+                 
                         <div class="caption">
-                            <p align="center"><a href="login.php" class="btn btn-info btn-lg"><b>
-                                        <font size="6">登入</a></font></b></p>
+                            <p align="center"><a href="login.php" class="btn btn-warning btn-lg"  style="width: 300px ;height: 90px;"><b>
+                                        <font size="10">登入</a></font></b></p>
                         </div>
-                    </div>
+                   
                 </div>
-                <div class="clearfix visible-md visible-xs"></div>
-                <div class="col-lg-6 col-xs-8">
-                    <div class="thumbnail">
+                <div class="clearfix visible-md visible-lg"></div>
+                <div class="col-lg-16 col-xs-18">
+                    
                         <div class="caption">
-                            <p align="center"><a href="register.php" class="btn btn-info btn-lg"><b>
-                                        <font size="6">註冊</a></font></b></p>
+                            <p align="center"><a href="register.php" class="btn btn-danger   btn-lg " style="width: 300px ;height: 90px;"><b>
+                                        <font size="10">註冊</a></font></b></p>
                         </div>
-                    </div>
+                
                 </div>
             </div><!-- end nested row 3a -->
         </div>
 
-        <!--底部-->
-        <footer class="fixed-bottom text-center text-lg-start">
-            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-                中國文化大學畢業專題製作B-07組
-            </div>
-        </footer>
+
+        
     </div>
     <!-- javascript -->
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
