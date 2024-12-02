890f129815d1a71f85cf5bb1fa095a5c2ae29392
yuenliu <101539991+yuenliu@users.noreply.github.com>
2024-12-03 00:57:44 +0800

    Update member_center.php

--- PHP/member_center.php
+++ PHP/member_center.php
@@ -1,8 +1,11 @@
 <?php
-session_start();
-require_once("login_check.php");
+    include("navbar.php");
+    session_start();
+    require_once("login_check.php");
 ?>
 
+
+<!DOCTYPE html>
 <html>
 
 <head>
@@ -13,22 +16,19 @@ require_once("login_check.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js
-"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/member_center.css">
 </head>
 
-</html>
-
 <body>
-    <?php
-    include("navbar.php");
-    ?>
-    <div class="col-sm-4 col-sm-offset-4">
-        <div class="panel panel-default">
-            <div class="panel-heading">
+        <!--內容-->
+        <div class="col-sm-4 col-sm-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
                 <h1>
-                    <?php
+                     <?php
                     require_once("database.php");
                     session_start();
                     $query_RecMember = "SELECT * FROM `member` WHERE `account`='" . $_SESSION["account"] . "'";
@@ -38,21 +38,21 @@ require_once("login_check.php");
                     if ($row_Recmember["identity"] == "consumer") {
                         echo "會員中心";
                     } else if ($row_Recmember["identity"] == "store") {
-                        echo "店家中心";
+                        echo "會員中心";
                     } else if ($row_Recmember["identity"] == "root") {
                         echo "儀錶板";
                     }
                     ?>
                 </h1>
-            </div>
-            <div class="panel-body">
-                <?php
+                </div>
+                <div class="panel-body">
+                <?php 
                 if ($row_Recmember["identity"] == "consumer") {
                     echo "<p><h2>會員名稱：<strong>";
                     echo $row_Recmember["name"];
                     echo "</strong></p>";
                 } else if ($row_Recmember["identity"] == "store") {
-                    echo "<p>店家名稱：<strong>";
+                    echo "<p><h2>負責人名稱：<strong>";
                     echo $row_Recmember["name"];
                     echo "</strong></p>";
                 } else if ($row_Recmember["identity"] == "root") {
@@ -61,19 +61,25 @@ require_once("login_check.php");
                 ?>
                 <br>
                 <p align="center">
-                    <?php
+                     <?php
                         if ($row_Recmember["identity"] == "consumer") {
                             echo "<a href='member_updateform.php'> 修改會員資料 </a>";
                         } else if ($row_Recmember["identity"] == "store") {
-                            echo "<a href='store_updateform.php'> 修改店家資料 </a>";
+                            echo "<a href='member_updateform.php'> 修改會員資料 </a>";
                         }
                     ?>
                     <br><br>
                     <a href="member_change_password.php"> 修改密碼 </a>
                     <br><br>
-                    <a href="logout.php"> 登出系統 </a><br>
+                    <a href="logout.php"> 登出系統 </a><br>              
                 </p>
+                </div>
+                </div>
             </div>
         </div>
-    </div>
+    <!-- javascript -->
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
+
+</html>
