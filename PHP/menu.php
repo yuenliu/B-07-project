a60251f8e827ae9bff1cbe6782cdcf26f7d6c134
a25458417584 <101554283+a25458417584@users.noreply.github.com>
2024-12-10 00:01:55 +0800

    Update menu.php

--- PHP/menu.php
+++ PHP/menu.php
@@ -8,8 +8,9 @@
     <title>文大線上點餐系統</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 
@@ -18,6 +19,45 @@
         <?php include("navbar.php"); ?>
         <div class="container">
             <?php
+            if (isset($_POST["add_to_cart"])) {
+                $foodid = $_POST["foodid"];
+                $remark = $_POST["remark"];
+                $quantity = $_POST["quantity"];
+
+                session_start();
+                $sql_query = "SELECT * FROM `member` WHERE `account` = '" . $_SESSION["account"] . "'";
+                $result = mysqli_query($conn, $sql_query);
+                $row = mysqli_fetch_assoc($result);
+                $member_id = $row["id"];
+
+                $sql_cart = "SELECT * FROM `cart` WHERE `food_id` = '$foodid' AND `member_id` = '$member_id' ";
+                $cart_result = mysqli_query($conn, $sql_cart);
+                if (mysqli_num_rows($cart_result) == 0) {
+                    $sql = "INSERT INTO `cart` (`member_id`, `food_id`, `quantity`, `remark`) VALUES (?, ?, ?, ?)";
+                    $stmt = mysqli_stmt_init($conn);
+                    $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
+                    if ($prepareStmt) {
+                        mysqli_stmt_bind_param($stmt, "ssss", $member_id, $foodid, $quantity, $remark);
+                        mysqli_stmt_execute($stmt);
+                        echo "<div class='alert alert-success'>加入成功！</div>";
+                    } else {
+                        die("發生了一些錯誤！請洽管理員。");
+                    }
+                } else {
+                    $row_cart = mysqli_fetch_assoc($cart_result);
+                    $new_qty = $row_cart["quantity"] + $quantity;
+                    $sql_update = "UPDATE `cart` SET `quantity` = ?, `remark` = ? WHERE `food_id` = '$foodid' AND `member_id` = '$member_id' ";
+                    $stmt = mysqli_stmt_init($conn);
+                    $prepareStmt = mysqli_stmt_prepare($stmt, $sql_update);
+                    if ($prepareStmt) {
+                        mysqli_stmt_bind_param($stmt, "ss", $new_qty, $remark);
+                        mysqli_stmt_execute($stmt);
+                        echo "<div class='alert alert-success'>追加成功！</div>";
+                    } else {
+                        die("發生了一些錯誤！請洽管理員。");
+                    }
+                }
+            }
             session_start();
             if (isset($_GET['storeid'])) {
                 $id = $_GET['storeid'];
@@ -71,7 +111,6 @@
                 if ($counter > 0 && $counter % 3 == 0) {
                     echo "</div><div class='rowfood'>";
                 }
-                echo "<a href='menu.php?storeid=" . $row["id"] . "&foodid=" . $rowfood["food_id"] . "'>";
                 echo "<div class='col panel panel-default col-md-4'>";
                 echo "<div class='panel-heading'><img src='foodimg/" . $rowfood["food_image"] . "' style='width:300px; height:200px;' /></div>";
                 echo "<table class='table'>";
@@ -79,8 +118,38 @@
                 echo "<th>";
                 echo "<th>價錢 : " . $rowfood["food_price"] . "</th>";
                 echo "</th>";
-                echo "</table></div></a>";
+                echo "<th><button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#foodid_" . $rowfood["food_id"] . "'>查看詳情</button></th>";
+                echo "</table></div>";
                 $counter++;
+            }
+            echo "</div>";
+            $sql_query = "SELECT `food_id`,`food_image`,`food_name`,`food_price` FROM `food` WHERE `store_id` = $store_id ";
+            $result = mysqli_query($conn, $sql_query);
+            echo "<div class='rowmodal'>";
+            while ($rowfood = mysqli_fetch_assoc($result)) {
+                echo "<div class='modal fade' id='foodid_" . $rowfood["food_id"] . "' role='dialog'>";
+                echo "<div class='modal-dialog'>
+                        <!-- Modal content-->
+                        <div class='modal-content'>
+                            <div class='modal-header'>
+                                <button type='button' class='close' data-dismiss='modal'>&times;</button>
+                                <h4 class='modal-title'>確認內容</h4>
+                            </div>
+                        <form action='menu.php?storeid=" . $store_id . "' method='post'><div class='modal-body'>";
+                echo "<input type='hidden' name='foodid' value=" . $rowfood["food_id"] . ">";
+                echo "<p>餐點名稱：" . $rowfood["food_name"] . "</p>";
+                echo "<label>數量</label><input type='number' name='quantity' value='1' min='1' max='50'><br>";
+                echo "<label>備註</label><input type='text' name='remark'>";
+                echo "<p class='right'>單價：" . $rowfood["food_price"] . "$</p>";
+                echo "</div>
+                <div class='modal-footer'>
+                  <button type='button' class='btn btn-warning' data-dismiss='modal'>返回</button>
+                  <input type='submit' class='btn btn-success' name='add_to_cart' value='加入購物車'>
+                        </div></form>
+                    </div>
+                    </div>
+                    </div>
+                    </div>";
             }
             echo "</div>";
             ?>
